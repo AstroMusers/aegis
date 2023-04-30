cf70e084b968ce4d05cb59841beb87dad8f26901
Asaf Kaya <asafka7a@gmail.com>
2023-04-30 10:40:41 +0300

    Added a 2D visualization of the change in L_r by a and B-s.

--- radilo-lum_2.py
+++ radilo-lum_2.py
@@ -3,45 +3,32 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 mpl.rcParams["figure.autolayout"] = True
-plt.rcParams['figure.figsize'] = [4, 6]
+plt.rcParams['figure.figsize'] = [5, 3]
 
-rc = {"font.family" : "times new roman",
-      "font.size" : 11,
-      "mathtext.fontset" : "stix"}
+rc = {"font.family": "times new roman",
+      "font.size": 11,
+      "mathtext.fontset": "stix"}
 plt.rcParams.update(rc)
 
 mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=["navy", "darkred", "b"])
 
-k = 7.39 * 10**22
+k = 7.39 * 10 ** 22
 
-plt.figure(1)
-
-v_sw = np.sqrt(1000) # km/s
-B_sw = 10**(-8) # teslas
+v_sw = np.sqrt(1000)  # km/s
+B_sw = 10 ** (-8)  # teslas
 B_s = np.linspace(1, 50, 1000)
-a = 5
-
-L_r = k * (B_s)**(2/3) * a * v_sw * B_sw**2
-
-plt.subplot(2,1,1)
-plt.plot(B_s, L_r)
-plt.xlabel(r"$B_s$ $(G)$")
-plt.ylabel(r"$L_r$ (Watts)")
-plt.title(r"Scaling of $L_r$ by $B_s$")
-
-v_sw = np.sqrt(1000) # km/s
-B_sw = 10**(-8) # teslas
-B_s = 10
 a = np.linspace(0.1, 10, 1000)
 
-L_r = k * (B_s)**(2/3) * a * v_sw * B_sw**2
+B_s, a = np.meshgrid(B_s, a)
 
-plt.subplot(2,1,2)
-plt.plot(a, L_r)
-plt.xlabel(r"$a$ $(AU)$")
-plt.ylabel(r"$L_r$ (Watts)")
-plt.title(r"Scaling of $L_r$ by $a$")
+L_r = k * B_s ** (2 / 3) * a ** (2 / 3) * v_sw * B_sw ** 2
 
+fig, ax = plt.subplots(1, 1)
+# cp = ax.contourf(X, Y, L_r)
+cp = plt.imshow(L_r, cmap=plt.cm.viridis, origin="lower",
+                extent=[B_s.min(), B_s.max(), a.min(), a.max()], aspect="auto")
+fig.colorbar(cp, label=r"$L_r$ (Watts)")  # Add a colorbar to a plot
+ax.set_title('Scaling of $L_r$ by $B_s$ and $a$')
+ax.set_xlabel(r'$B_s$ (G)')
+ax.set_ylabel(r'$a$ (AU)')
 plt.show()
-plt.close(1)
-

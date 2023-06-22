33e754c81ca0226fe3c2b95fc6c85c1361b0a7a4
Asaf Kaya <asafka7a@gmail.com>
2023-06-22 10:51:53 +0300

    Added a simple comment

--- synthetic sample.py
+++ synthetic sample.py
@@ -15,6 +15,7 @@ mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=["firebrick", "navy", "b"])
 
 rng = np.random.default_rng()
 
+
 class Exoplanet:
     def __init__(self, semi_major_axis, magnetic_field, star_mass, star_mass_loss, distance):
         self.semi_major_axis = semi_major_axis
@@ -29,13 +30,13 @@ class Exoplanet:
 
 exoplanets = []
 for i in range(100):
-    a = rng.normal(0.5, 0.1)  # AU
+    a = rng.normal(0.5, 0.2)  # AU
     Bs = rng.normal(15, 5)  # gauss
     # Bs = rng.random() * 20  # gauss
     M = rng.normal(0.8, 0.3)  # solar masses
-    Mdot = rng.normal(68, 10)  # e-15 solar masses
+    Mdot = rng.normal(68, 10)  # e-15 solar masses / yr
     D = rng.normal(1000, 400)  # light years
-    D = D* 9.46 * 10**15 # conversion to meters
+    D = D * 9.46 * 10 ** 15  # conversion to meters
     exo = Exoplanet(a, Bs, M, Mdot, D)
     exoplanets.append(exo)
 
@@ -47,7 +48,7 @@ Rj = 1  # Rj
 Rmpj = 60  # Rj
 M_sun = 1  # M_sun
 M_sun_loss = 68  # e-15 solar masses
-nuJ = 24 #MHz
+nuJ = 24  # MHz
 
 v_sw = 400  # km/s
 B_sw = 10 ** (-8)  # teslas
@@ -56,39 +57,62 @@ delta = 10.5
 
 frequencies = []
 intensities = []
+distances = []
+semis = []
 
 for exo in exoplanets:
-
     a = exo.semi_major_axis
     B = exo.magnetic_field
     M = exo.star_mass
     Mdot = exo.star_mass_loss
     D = exo.distance
 
+    semis.append(a)
+
+    distances.append(D / (9.46 * 10 ** 15))
+
     nu = nuJ * B / Bj
     frequencies.append(nu)
 
-    Rmp = 56*Rj* ( (B/Bj) / (aj/a) )**(1/3)
+    Rmp = 56 * Rj * ((B / Bj) / (aj / a)) ** (1 / 3)
     # Lj = 7.4 * 10**22 * Bj**(2/3) * a**(2/3) * v_sw * B_sw**2
-    Lj = 2.1 * 10**11
+    Lj = 2.1 * 10 ** 11
 
-    L = (Rmp / Rmpj)**2 * (aj / a)**2 * (M/M_sun) * (Mdot/M_sun_loss) * Lj
+    L = (Rmp / Rmpj) ** 2 * (aj / a) ** 2 * (M / M_sun) * (Mdot / M_sun_loss) * Lj
     # print(L)
 
     # I = delta*L / (4*np.pi*D)**2 / nu * 10**(26)
-    I = delta*L / (4*np.pi*nu*D)**2 * 10**(26)
+    I = delta * L / (4 * np.pi * nu * D) ** 2 * 10 ** (26)
     intensities.append(I)
 
 frequencies = np.array(frequencies)
-frequencies *= 10**6
+# frequencies *= 10**6
+
+distances = np.array(distances)
+
+semis = np.array(semis)
+semis = np.reciprocal(semis)
+semis *= 20
 
 df = pd.DataFrame({"x": frequencies,
-                   "y": intensities})
+                   "y": intensities,
+                   "d": distances,
+                   "s": semis})
 
-plt.scatter(df.x, df.y)
-plt.yscale("log")
-plt.xscale("log")
-plt.ylabel("Radio Intensity (Jy)")
-plt.xlabel("Emission Frequency (Hz)")
-plt.title("Frequency and Intensity of CMI Emissions of a Synthetic Exoplanet Sample")
+fig, ax = plt.subplots()
+
+im = ax.scatter(df.x, df.y, c=df.d, s=df.s, cmap="viridis")
+plt.axvline(x=10, color="black", linestyle="dashed")
+
+fig.colorbar(im, ax=ax, label="Distance from Earth (ly)")
+
+ax.set_xscale("log")
+ax.set_yscale("log")
+ax.set_xlim(left=8)
+
+ax.axvspan(8, 10, alpha=0.2, color="teal")
+
+ax.set_title("Frequency and Intensity of CMI Emissions of a Synthetic Exoplanet Sample")
+ax.set_xlabel("Emission Frequency (MHz)")
+ax.set_ylabel("Radio Brightness (Jy)")
 plt.show()

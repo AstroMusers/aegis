6e25050a732e4d2984a5f9934205154c16c924be
Asaf Kaya <asafka7a@gmail.com>
2023-06-23 23:47:39 +0300

    Defined functions for most operations of the CMI predicting parts of the code, included docstrings. Added assertions for physical meaning and played with distribution parameters a little. Fixed an error in the calculation of radio brightness caused by unit mismatch (it used MHz instead of Hz).

--- synthetic sample.py
+++ synthetic sample.py
@@ -28,32 +28,103 @@ class Exoplanet:
         return f"Exoplanet with: a={self.semi_major_axis}, Bs={self.magnetic_field}, M={self.star_mass}, Mdot={self.star_mass_loss}, D={self.distance} \n"
 
 
+def max_freq(B):
+    """
+    Finds the maximum CMI emission frequency based on Jupiter's emission, as given in Ashtari et al. 2022.
+    :param B: Magnetic Field Strength at the surface of the exoplanet, must be given in Gauss.
+    :return: Maximum CMI emission frequency of the exoplanet, given in Hz
+    """
+    Bj = 14  # gauss
+    nuJ = 24 * 10**6  # Hz
+    return nuJ * B / Bj
+
+
+def magnetopause(B, a):
+    """
+    Finds the magnetopause standoff distance of an exoplanet using its magnetic field strength
+    and semi-major axis. The scaling equation in Ashtari et al. 2022 is used
+    :param B: Magnetic field strength given in Gauss.
+    :param a: Semi-major axis given in AU.
+    :return: The magnetopause standoff distance of the exoplanet given in Jupiter radii.
+    """
+    Bj = 14  # gauss
+    aj = 5.204  # AU
+    Rj = 1  # Rj
+    return 56 * Rj * ((B / Bj) / (aj / a)) ** (1 / 3)
+
+
+def radio_lum(Rmp, a, M, Mdot):
+    """
+    Calculates the total output power of CMI emission of an exoplanet using its semi-major axis,
+    The host star's mass and mass loss rate. The equation is given in Ashtari et al. 2022
+    :param Rmp: Magnetopause standoff distance given in Jupiter Radii.
+    :param a: Semi-major axis given in AU
+    :param M: Host star's mass in solar masses.
+    :param Mdot: Host star's mass loss rate in 10^-15 solar masses per year.
+    :return: The total output power of the CMI emission, or the radio brightness of the exoplanet
+            given in Watts.
+    """
+    Rmpj = 60  # Rj
+    M_sun = 1  # M_sun
+    M_sun_loss = 68  # e-15 solar masses
+    Lj = 2.1 * 10 ** 11
+    aj = 5.204  # AU
+    return (Rmp / Rmpj) ** 2 * (aj / a) ** 2 * (M / M_sun) * (Mdot / M_sun_loss) * Lj
+
+
+def radio_brightness(L, nu, D):
+    """
+    Calculates the beamed radio brightness of an exoplanet using its radio output power,
+    emission frequency and observation distance.
+    :param L: Total radio output power given in Watts.
+    :param nu: Emission frequency of the exoplanet given in Hz.
+    :param D: Distance to exoplanet given in meters.
+    :return: Spectral Radio Flux density or Radio Brightness of the exoplanet in Janskies(Jy).
+    """
+    delta = 10.5
+    return delta * L / (4 * np.pi * nu * D ** 2) * 10 ** 26
+
+
+def complete(B, a, M, Mdot, D):
+    """
+    Finds the CMI radio brightness of the exoplanet directly using initial parameters.
+    :param B: Magnetic field strength given in Gauss.
+    :param a: Semi-major axis given in AU
+    :param M: Host star's mass in solar masses.
+    :param Mdot: Host star's mass loss rate in 10^-15 solar masses per year.
+    :param D: Distance to exoplanet given in meters.
+    :return: Spectral Radio Flux density or Radio Brightness of the exoplanet in Janskies(Jy).
+    """
+    nu = max_freq(B)
+    Rmp = magnetopause(B, a)
+    L = radio_lum(Rmp, a, M, Mdot)
+    I = radio_brightness(L, nu, D)
+    return I
+
+
 exoplanets = []
 for i in range(150):
     a = 4 * rng.random() - 2  # logAU
-    Bs = rng.normal(10, 4)  # gauss
-    # Bs = rng.random() * 20  # gauss
-    M = rng.normal(0.8, 0.3)  # solar masses
+
+    Bs = rng.normal(12, 4)  # gauss
+    assert Bs >= 0, f"Magnetic Field Strength is expected nonnegative, instead got {Bs=}."
+
+    M = rng.normal(1.2, 0.4)  # solar masses
+    assert M > 0, f"Stellar Mass is expected positive, instead got {M=}."
+
     Mdot = rng.normal(68, 20)  # e-15 solar masses / yr
+    assert Mdot > 0, f"Stellar Mass Loss Rate is expected positive, instead got {Mdot=}."
+
     D = rng.normal(1000, 400)  # light years
-    D = D * 9.46 * 10 ** 15  # conversion to meters
+    assert D > 0, f"Distance to Exoplanet is expected positive, instead got {D=}."
+
     exo = Exoplanet(a, Bs, M, Mdot, D)
     exoplanets.append(exo)
 
 print(exoplanets)
 
-Bj = 14  # gauss
-aj = 5.204  # AU
-Rj = 1  # Rj
-Rmpj = 60  # Rj
-M_sun = 1  # M_sun
-M_sun_loss = 68  # e-15 solar masses
-nuJ = 24  # MHz
-
 v_sw = 400  # km/s
 B_sw = 10 ** (-8)  # teslas
-
-delta = 10.5
 
 frequencies = []
 intensities = []
@@ -68,22 +139,18 @@ for exo in exoplanets:
     D = exo.distance
 
     semis.append(a)
-    a = 10 ** a
+    distances.append(D)
 
-    distances.append(D / (9.46 * 10 ** 15))
+    a = 10 ** a  # conversion to AU
+    D = D * 9.46 * 10 ** 15  # conversion to meters
 
-    nu = nuJ * B / Bj
-    frequencies.append(nu)
+    nu = max_freq(B)
+    assert nu > 0, f"Maximum emission frequency must be positive, instead got {nu=}."
+    frequencies.append(nu/10**6)
 
-    Rmp = 56 * Rj * ((B / Bj) / (aj / a)) ** (1 / 3)
-    # Lj = 7.4 * 10**22 * Bj**(2/3) * a**(2/3) * v_sw * B_sw**2
-    Lj = 2.1 * 10 ** 11
 
-    L = (Rmp / Rmpj) ** 2 * (aj / a) ** 2 * (M / M_sun) * (Mdot / M_sun_loss) * Lj  # Ashtari 2022
-    # print(L)
-
-    # I = delta*L / (4*np.pi*D)**2 / nu * 10**(26)
-    I = delta * L / (4 * np.pi * nu * D ** 2) * 10 ** 26  # Ashtari 2022
+    I = complete(B, a, M, Mdot, D)
+    assert I > 0, f"Radio brightness must be positive, instead got {I=}."
     intensities.append(I)
 
 frequencies = np.array(frequencies)

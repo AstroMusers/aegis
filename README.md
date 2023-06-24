b0182fdc4127c683bb324ab7efac0cbeb278a9a6
Asaf Kaya <asafka7a@gmail.com>
2023-06-24 12:21:37 +0300

    Created the GUI with the ability to save recent parameters. Hats off to ChatGPT!

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 # Radio Signatures of Exoplanets
 
-This repository is a comprehensive one containin anything I do as part of the Radio Signatures of Exoplanets research project led by Tansu Daylan.
+This repository is a comprehensive one containing anything done as 
+part of the Radio Signatures of Exoplanets research project led by 
+Tansu Daylan. The codes use scientific knowledge acquired from various
+research papers and books, most notably the paper 
+"Detecting Magnetospheric Radio Emission from Giant Exoplanets" by 
+Ashtari et al. 2022.
 
+### Scaling Scripts
+The Python scripts "radio-lum_2.py" and "scaling_of_radio_luminosity.py" 
+serve the purpose of visualizing the scaling of radio luminosity of
+exoplanets with some of their parameters. These are expected not to 
+have any direct contribution to any research paper that might be published.
+
+### The Module
+The script "radio_module.py" provides a new module with an exoplanet class
+and various functions that might be used throughout the project. 
+
+### Expectation Script and the GUI.
+The scripts "exoplanet_predictions.py" and "GUI_for_predictions.py" are
+currently under development. The former is the main script that generates
+a random sample of exoplanets by randomly assigning values drawn from
+specific probability distributions to various characteristics of exoplanets.
+It then plots the expectations of CMI radiation frequency and radio
+brightnesses of the drawn sample.
---
+++ exoplanet_predictions.py
@@ -0,0 +1,142 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib as mpl
+import pandas as pd
+import sys
+
+from radio_module import *
+
+
+# Matplotlib Configuration
+# ------------------------
+mpl.rcParams["figure.autolayout"] = True
+plt.rcParams['figure.figsize'] = [8, 4]
+
+rc = {"font.family": "times new roman",
+      "font.size": 11,
+      "mathtext.fontset": "stix"}
+plt.rcParams.update(rc)
+
+mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=["firebrick", "navy", "b"])
+
+# Random number generator
+rng = np.random.default_rng()
+
+# Get the input variables from command-line arguments. Use this if you want
+# to run the code using the GUI, otherwise comment out the following
+# 10 lines.
+a_min = float(sys.argv[1])
+a_max = float(sys.argv[2])
+B_mean = float(sys.argv[3])
+B_sd = float(sys.argv[4])
+M_mean = float(sys.argv[5])
+M_sd = float(sys.argv[6])
+Mdot_mean = float(sys.argv[7])
+Mdot_sd = float(sys.argv[8])
+D_mean = float(sys.argv[9])
+D_sd = float(sys.argv[10])
+
+# Default values for the variables. Use this section of the code to run
+# the code in the script.
+# --------------------------------------------------------------------
+# a_min = -2
+# a_max = 2
+# B_mean = 10
+# B_sd = 3
+# M_mean = 3
+# M_sd = 0.8
+# Mdot_mean = 100
+# Mdot_sd = 30
+# D_mean = 1000
+# D_sd = 300
+# --------------------------------------------------------------------
+
+# Create Synthetic Sample
+# -----------------------
+exoplanets = []
+for i in range(150):
+    a = rng.uniform(a_min, a_max)  # logAU
+
+    Bs = rng.normal(B_mean, B_sd)  # gauss
+    assert Bs >= 0, f"Magnetic Field Strength is expected nonnegative, instead got {Bs=}."
+
+    M = rng.normal(M_mean, M_sd)  # solar masses
+    assert M > 0, f"Stellar Mass is expected positive, instead got {M=}."
+
+    Mdot = rng.normal(Mdot_mean, Mdot_sd)  # e-15 solar masses / yr
+    assert Mdot > 0, f"Stellar Mass Loss Rate is expected positive, instead got {Mdot=}."
+
+    D = rng.normal(D_mean, D_sd)  # light years
+    assert D > 0, f"Distance to Exoplanet is expected positive, instead got {D=}."
+
+    exo = Exoplanet(a, Bs, M, Mdot, D)
+    exoplanets.append(exo)
+
+print(exoplanets)
+
+# Currently unused parameters
+v_sw = 400  # km/s
+B_sw = 10 ** (-8)  # teslas
+
+# Make Calculations
+# -----------------
+frequencies = []
+intensities = []
+distances = []
+semis = []
+
+for exo in exoplanets:
+    a = exo.semi_major_axis
+    B = exo.magnetic_field
+    M = exo.star_mass
+    Mdot = exo.star_mass_loss
+    D = exo.distance
+
+    semis.append(a)
+    distances.append(D)
+
+    a = 10 ** a  # conversion to AU
+    D = D * 9.46 * 10 ** 15  # conversion to meters
+
+    nu = max_freq(B)
+    assert nu > 0, f"Maximum emission frequency must be positive, instead got {nu=}."
+    frequencies.append(nu/10**6)
+
+    I = complete(B, a, M, Mdot, D)
+    assert I > 0, f"Radio brightness must be positive, instead got {I=}."
+    intensities.append(I)
+
+frequencies = np.array(frequencies)
+
+distances = np.array(distances)
+distances = np.reciprocal(distances)
+distances *= 10 ** 4
+
+semis = np.array(semis)
+
+# Plotting
+# -------------------
+
+df = pd.DataFrame({"x": frequencies,
+                   "y": intensities,
+                   "d": distances,
+                   "s": semis})
+
+
+fig, ax = plt.subplots()
+
+im = ax.scatter(df.x, df.y, c=df.s, s=df.d, cmap="jet_r")
+plt.axvline(x=10, color="black", linestyle="dashed")
+
+fig.colorbar(im, ax=ax, label="Distance to Host Star ($\log_{10}{\mathrm{(AU)}}$)")
+
+ax.set_xscale("log")
+ax.set_yscale("log")
+ax.set_xlim(left=6)
+
+ax.axvspan(0, 10, alpha=0.2, color="teal")
+
+ax.set_title("Frequency and Intensity of CMI Emissions of a Synthetic Exoplanet Sample")
+ax.set_xlabel("Emission Frequency (MHz)")
+ax.set_ylabel("Radio Brightness (Jy)")
+plt.show()
